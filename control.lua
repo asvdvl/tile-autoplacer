d0952cdcd17d5be1947bc4aad73a3c6c0d2df21d
asvdev <57688070+asvdvl@users.noreply.github.com>
2024-05-09 15:23:45 +0300

    first way to place tiles(require tiles in cursor)

--- control.lua
+++ control.lua
@@ -1,9 +1,9 @@
-local tile = {name="refined-concrete", position={}}
+local tile_name = "refined-concrete"
 
 local function on_player_changed_position(event)
     local player = game.get_player(event.player_index)
-    tile.position = player.position
-    player.surface.set_tiles({tile}, false, false)
+
+    player.build_from_cursor{position=player.position}
 end
 
 script.on_event(defines.events.on_player_changed_position, on_player_changed_position)
