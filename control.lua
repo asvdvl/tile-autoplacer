0e64c1fea8e7af56982f27a222a88de4218083b9
asvdev <57688070+asvdvl@users.noreply.github.com>
2024-05-12 02:47:12 +0300

    added filter for tile

--- control.lua
+++ control.lua
@@ -33,8 +33,14 @@ local function on_player_changed_position_logic(event)
         local dont_allow_to_build_tile = player.surface.get_tile(player.position.x, player.position.y).prototype.mineable_properties.minable and get_setting_value("dont-build-if-on-tile", player)
         local hand_not_empty = player.cursor_stack.valid_for_read
         local is_tile_in_hand = hand_not_empty and item_is_tile(player.cursor_stack.prototype.name)
+        local filter_item_name = #get_setting_value("work-only-with-specific-tile", player) > 0 and get_setting_value("work-only-with-specific-tile", player)
 
-        if dont_allow_to_build_tile or (hand_not_empty and not is_tile_in_hand) then
+        if filter_item_name and not game.item_prototypes[filter_item_name] then
+            player.print({"?", {"message.tile-autoplacer-item-not-found", filter_item_name}})
+            return
+        end
+
+        if dont_allow_to_build_tile or (hand_not_empty and not is_tile_in_hand) or (is_tile_in_hand and filter_item_name and is_tile_in_hand.name ~= filter_item_name) then
             --if there is no building permit or there is something else in the cursor, we skip the rest of the logic
             return
         end
@@ -43,13 +49,19 @@ local function on_player_changed_position_logic(event)
             local inventory = player.get_main_inventory().get_contents()
             local best_tile = {name = "", speed = 0}
 
-            for key in pairs(inventory) do
-                local place_result = game.item_prototypes[key].place_as_tile_result and game.item_prototypes[key].place_as_tile_result.result
-                if place_result and place_result.valid and place_result.walking_speed_modifier and best_tile.speed < place_result.walking_speed_modifier then
-                    best_tile.name = key
-                    best_tile.speed = place_result.walking_speed_modifier
-                    log(tostring(key)..tostring(place_result.walking_speed_modifier))
+            if not filter_item_name then
+                for key in pairs(inventory) do
+                    local place_result = game.item_prototypes[key].place_as_tile_result and game.item_prototypes[key].place_as_tile_result.result
+                    if place_result and place_result.valid and place_result.walking_speed_modifier and best_tile.speed < place_result.walking_speed_modifier then
+                        best_tile.name = key
+                        best_tile.speed = place_result.walking_speed_modifier
+                        log(tostring(key)..tostring(place_result.walking_speed_modifier))
+                    end
                 end
+            elseif player.get_main_inventory().find_item_stack(filter_item_name) then
+                best_tile.name = filter_item_name
+            else
+                return
             end
 
             if #best_tile.name > 0 then
@@ -84,7 +96,7 @@ end
 local function on_player_changed_position(event)
     local success, reason = pcall(on_player_changed_position_logic, event)
     if not success then
-        local message = "error while handling event: "..reason
+        local message = "error while handling event: "..reason.."; event data: "..serpent.block(event)
         log(message)
         game.print(message)
     end
